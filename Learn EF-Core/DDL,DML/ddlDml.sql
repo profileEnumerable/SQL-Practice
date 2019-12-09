9ff3d37341c5a1a1a18a5fe496c05a7287a07c6f
profileEnumerable <fustethernet@gmail.com>
2019-12-09 11:30:17 +0200

    #2 - interaction with table columns

--- Learn EF-Core/DDL,DML/ddlDml.sql
+++ Learn EF-Core/DDL,DML/ddlDml.sql
@@ -3,13 +3,13 @@
 --|--------Interaction with DATABASES--------|--
 --Create Database
 CREATE DATABASE EStoreDB ON PRIMARY
-    ( NAME = N'EStoreDB', FILENAME = N'fileName' , SIZE = 167872 KB , MAXSIZE = UNLIMITED, FILEGROWTH = 16384 KB )
+    ( NAME = N'EStoreDB', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\EStoreDB.mdf' , SIZE = 167872 KB , MAXSIZE = UNLIMITED, FILEGROWTH = 16384 KB )
     LOG ON
-    ( NAME = N'EStoreDB_Log', FILENAME = N'fileName' , SIZE = 2048 KB , MAXSIZE = 2048 GB , FILEGROWTH = 16384 KB )
+    ( NAME = N'EStoreDB_Log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATA\EStoreDB_Log.ldf' , SIZE = 2048 KB , MAXSIZE = 2048 GB , FILEGROWTH = 16384 KB )
 
 --Alter Database
 ALTER DATABASE EStoreDB
-COLLATE Cyrillic_General_CI_AS
+    COLLATE Cyrillic_General_CI_AS
 
 --Move to the EStoreDB database
 use EStoreDB
@@ -21,14 +21,23 @@ DROP DATABASE EStoreDB
 
 --Create TABLE
 
-CREATE TABLE Goods(
-    id int NOT NULL IDENTITY,
-    Name varchar(35) NOT NULL,
-    Cost decimal NOT NULL,
-    Weight int    
+CREATE TABLE Goods
+(
+    id     int         NOT NULL IDENTITY,
+    Name   varchar(35) NOT NULL,
+    Cost   decimal     NOT NULL,
+    Weight int
 );
 
 --Alter table column
 
 ALTER TABLE Goods
-ALTER COLUMN Weight int NOT NULL 
+    ALTER COLUMN Weight int NOT NULL
+
+--Add table column 
+ALTER TABLE Goods
+    ADD Sale decimal
+
+--Drop table column
+ALTER TABLE Goods
+    DROP COLUMN Sale
