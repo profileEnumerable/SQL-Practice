6c5ac82630b3e974201e3cfc162725307475276d
profileEnumerable <fustethernet@gmail.com>
2019-12-28 17:17:20 +0200

    #2 - Basic DML commands

--- T-SQL Practice/ddlDml.sql
+++ T-SQL Practice/ddlDml.sql
@@ -20,7 +20,6 @@ DROP DATABASE EStoreDB
 --|--------Interaction with TABLES--------|--
 
 --Create TABLE
-
 CREATE TABLE Goods
 (
     id     int         NOT NULL IDENTITY,
@@ -42,15 +41,13 @@ ALTER TABLE Goods
 ALTER TABLE Goods
     DROP COLUMN Sale
 
-/*>>>>>>>>>>DML<<<<<<<<<<*/
-
---INSERT data to TABLE
+/*>>>>>>>>>>Transact-SQL:DML<<<<<<<<<<*/
 use EStoreDB
 
---Basic INSERT 
+--Basic /INSERT/ 
 -- We should dont specified keyword: INTO
-INSERT Goods
-(Name, Cost, Weight)
+INSERT INTO Goods
+    (Name, Cost, Weight)
 VALUES ('SmartTV', 50000, 17),
        ('Headphones', 3000, 0.3),
        ('Purse', 1000, 0.1)
@@ -59,11 +56,24 @@ VALUES ('SmartTV', 50000, 17),
 SET IDENTITY_INSERT Goods ON
 
 INSERT INTO Goods
-(id, Name, Cost, Weight)
+    (id, Name, Cost, Weight)
 VALUES (50, 'Graphic Card', 6500, 0.45)
 
 SET IDENTITY_INSERT Goods OFF
 
+--INSERT from selected data
+CREATE TABLE SoftwareGoods
+(
+    Id   int,
+    Name varchar(35),
+    Cost decimal(18)
+);
+
+INSERT INTO SoftwareGoods
+SELECT Id, Name, Cost
+FROM Goods
+
+--Basic /SELECT/
 --SELECT all the rows
 SELECT *
 FROM Goods
@@ -76,3 +86,34 @@ FROM Goods
 SELECT [Name]
 FROM Goods
 WHERE id = 1
+
+
+--Basic /UPDATE/
+--#1 UPDATE specific record/s in TABLE  
+UPDATE Goods
+SET Sale = 70
+WHERE Name = 'SmartTV'
+
+--#2 UPDATE all table records namely: Sale fields
+UPDATE Goods
+SET Sale = 1
+
+
+--#3 UPDATE "Cost" field herewith using data from another("SoftwareGoods") table 
+UPDATE Goods
+SET Cost = sg.Cost
+FROM Goods AS g
+         JOIN SoftwareGoods AS sg ON g.id = sg.Id
+ 
+--Basic /DELETE/
+--DELETE specific row
+DELETE Goods
+WHERE id = 52
+
+--DELETE all row herewith dont reset the IDENTITY value for field: "id"  
+DELETE Goods
+
+--Basic /TRUNCATE/ 
+--TRUNCATE TABLE
+TRUNCATE TABLE Goods 
+TRUNCATE TABLE SoftwareGoods
