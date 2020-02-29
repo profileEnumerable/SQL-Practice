b067bf014ac5105900571ab9e1f98986f436772e
profileEnumerable <fustethernet@gmail.com>
2020-02-29 23:54:15 +0200

    BETWEEN/LIKE/Nested queries

--- #3. SELECT/Query-Scripts/WHERE.sql
+++ #3. SELECT/Query-Scripts/WHERE.sql
@@ -23,16 +23,63 @@ WHERE BirthDate !> '19950101'
 --3) Selection with multiple conditions
 SELECT *
 FROM Employees
-WHERE Department = 'Sales' AND Salary >= 6000
+WHERE Department = 'Sales'
+  AND Salary >= 6000
 
 --4) Selection that must satisfy at least one condition
 SELECT *
 FROM Employees
-WHERE Department = 'Sales' OR Department = 'supply'
+WHERE Department = 'sales'
+   OR Department = 'supply'
 
 SELECT *
 FROM Employees
 --WHERE Department = 'Sales' OR Department = 'supply' AND Salary >= 6000 #Note: wrong way,broken sequence,needs round brackets
-WHERE (Department = 'Sales' OR Department = 'supply') AND Salary >= 6000
+WHERE (Department = 'sales' OR Department = 'supply')
+  AND Salary >= 6000
 
---5) /IN/ searching for records that includes in predefined list of values
+--5) /IN/ searching for records that includes values from predefined list: 'sales', 'supply', 'law', 'logistics'
+SELECT *
+FROM Employees
+WHERE Department IN ('sales', 'supply', 'law', 'logistics')
+
+--/IN/ along with subquery that fetching range of values 
+--Note: interesting behavior, this query return 89 records
+SELECT *
+FROM Employees
+WHERE Salary IN (SELECT Salary FROM Employees)
+
+--6) /BETWEEN/ looks for records that contain a value from range
+--Simple query, without /BETWEEN/ operator  
+SELECT *
+FROM Employees
+WHERE BirthDate > '19850102'
+  AND BirthDate < '19870102'
+--With using /BETWEEN/ operator
+SELECT *
+FROM Employees
+WHERE BirthDate BETWEEN '19850102' AND '19870102'
+
+--7) /LIKE/ looks for a records, that contain value as a part of string 
+SELECT *
+FROM Employees
+WHERE Department LIKE 'sales'
+
+--Note: Wildcard characters (%, _, [], [^])
+--/%/ any amount of any characters
+SELECT *
+FROM Employees
+WHERE Phone LIKE '063%' 
+
+--/_/ any one character
+SELECT *
+FROM Employees
+WHERE Id LIKE '_2'
+
+--/[]/ range of characters
+--1) explicitly defined  range
+SELECT *
+FROM Employees
+WHERE Id LIKE '[2,4]2'
+
+--2) implicitly defined range
