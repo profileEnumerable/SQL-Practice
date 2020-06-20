1bfa693f9b5122587bcb35e5a4f87d3bf04720c2
profileEnumerable <fustethernet@gmail.com>
2020-06-20 23:17:16 +0300

    investigate common string functions

--- #4. Build-in Functions/Query-Scripts/string-functions.sql
+++ #4. Build-in Functions/Query-Scripts/string-functions.sql
@@ -17,5 +17,16 @@ SELECT LEFT('abcdefg', 3)  [LEFT],
        UPPER('abcdefg')    [UPPER],
        LEN('  123  ')      [LEN], --investigate!
        REVERSE('321')      [REVERSE]
+
+--Note STUFF: when we pass 0 as an amount of symbols to remove,the last arg 
+--inserting before specified start index 
+SELECT STUFF('adcdefg', 4, 2, 'New') [STUFF],
+       SUBSTRING('adcdefg', 4, 2)    [SUBSTRING]
+
+PRINT LTRIM('   string')
+PRINT RTRIM('functions   ')
+
+PRINT 'Move' + SPACE(3) + 'on'
+PRINT REPLICATE('R', 10)
        
        
